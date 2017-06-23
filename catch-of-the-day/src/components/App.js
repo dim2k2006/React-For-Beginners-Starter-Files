8cb4e62d79933a0ff675a64a4c390ad7da9ee42b
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-06-23 11:26:07 +0300

    lesson13 (Understanding State)

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+
+        // getInitialState
+        this.state = {
+            fishes: {},
+            order: {}
+        }
+    }
+
+    addFish(fish) {
+        // update state
+        const fishes = {...this.state.fishes};
+
+        // add new fish
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+
+        // set state
+        this.setState({fishes});
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -13,7 +37,7 @@ class App extends React.Component {
 
                 <Order />
 
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
