6c1f143a2cf03c3db69fb033bf07457352143391
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-06-24 12:50:30 +0300

    lesson18 (Persisting our State with Firebase)

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyD0EI7822uaxMFvzbtEA5KYE9dzN07Zp9c",
+    authDomain: "catch-of-the-day-5134c.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-5134c.firebaseio.com"
+});
+
+export default base;
